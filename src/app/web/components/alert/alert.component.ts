1e2087445be570f2b0fe85046a211f427e4721d2
Vitaliy Chorniy <vitaliychorniy@Vitaliys-MBP.fritz.box>
2016-10-27 07:34:26 +0200

    need to rethink arch for shared components for web and admin

---
+++ src/app/web/components/alert/alert.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'alert',
+  templateUrl: 'alert.component.html'
+})
+
+export class AlertComponent {
+  message: string;
+
+  constructor(private alertService: AlertService) { }
+
+  ngOnInit() {
+    this.alertService.getMessage()
+      .subscribe((message: string) => { this.message = message; });
+  }
+}
--- src/app/web/components/top-menu/top-menu.component.ts
+++ src/app/web/components/top-menu/top-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '../../../translate/index';
 
+import { User } from '../../models/user';
+
 @Component({
   moduleId: module.id,
   selector: 'top-menu',
@@ -10,8 +12,11 @@ import { TranslateService } from '../../../translate/index';
 
 export class TopMenuComponent implements OnInit{
   public selectedLang: string;
+  currentUser: User;
 
-  constructor(private _translate: TranslateService) { }
+  constructor(private _translate: TranslateService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
     this.selectedLang = this._translate.currentLang;
