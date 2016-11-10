cf7ed5e0ef4ccd70360239ae564b7ebd7153f3d4
Vitaliy Chorniy <chornij1@gmail.com>
2016-11-10 08:20:28 +0100

    almost finished

--- src/app/web/components/add-place/add-place-part1.component.ts
+++ src/app/web/components/add-place/add-place-part1.component.ts
@@ -15,6 +15,8 @@ export class AddPlacePart1Component implements OnInit {
 
     marker: any;
     places: any;
+    choosenPlace: any;
+    placeService: any;
     map: any;
     infoWindow: any;
     markers: any = [];
@@ -22,7 +24,7 @@ export class AddPlacePart1Component implements OnInit {
     autocompletePlaces: any;
     countryRestrict: any = { 'country': 'de' };
     defaultCountry: string = DEFAULT_COUNTRY;
-    MARKER_PATH: any =  MARKER_PATH;
+    MARKER_PATH: any = MARKER_PATH;
     countries: any = COUNTRIES;
 
     constructor() { }
@@ -38,6 +40,12 @@ export class AddPlacePart1Component implements OnInit {
         this.map = new google.maps.Map(document.getElementById('map'), mapProp);
         this.infoWindow = new google.maps.InfoWindow({ map: this.map });
         this.places = new google.maps.places.PlacesService(this.map);
+        this.placeService = new google.maps.places.PlacesService(this.map);
+
+        this.marker = new google.maps.Marker({
+            map: this.map,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -46,21 +54,9 @@ export class AddPlacePart1Component implements OnInit {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
                 };
-
-                this.marker = new google.maps.Marker({
-                    position: pos,
-                    map: this.map,
-                    draggable: true,
-                    animation: google.maps.Animation.DROP
-                });
-                this.marker.addListener('click', () => {
-                    if (this.marker.getAnimation() !== null) {
-                        this.marker.setAnimation(null);
-                    } else {
-                        this.marker.setAnimation(google.maps.Animation.BOUNCE);
-                    }
-                });
-
+                this.marker.setPosition(pos);
+                this.marker.setVisible(true);
+                this.map.setZoom(17);
                 this.map.setCenter(pos);
             }, () => {
                 this.handleLocationError(true, this.infoWindow, this.infoWindow.getCenter());
@@ -70,11 +66,12 @@ export class AddPlacePart1Component implements OnInit {
             this.handleLocationError(false, this.infoWindow, this.infoWindow.getCenter());
         }
         google.maps.event.addListener(this.map, 'click', (event: any) => {
-          this.marker.setPosition(event.latLng);
-          this.marker.setVisible(true);
-            //this.addMarker(event.latLng, this.map);
-        });
+            this.getPlacesByLocation({
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng()
+            }, this.map);
 
+        });
 
         this.autocomplete = new google.maps.places.Autocomplete(
                /** @type {!HTMLInputElement} */(
@@ -83,75 +80,18 @@ export class AddPlacePart1Component implements OnInit {
                 componentRestrictions: this.countryRestrict
             });
 
-        //PLACES_____________________________
         this.autocompletePlaces = new google.maps.places.Autocomplete(
           /** @type {!HTMLInputElement} */(document.getElementById('pac-input'))
         );
-
-        this.infoWindow = new google.maps.InfoWindow();
-        this.marker = new google.maps.Marker({
-            map: this.map,
-            anchorPoint: new google.maps.Point(0, -29)
-        });
-
         this.autocompletePlaces.bindTo('bounds', this.map);
-        this.autocompletePlaces.addListener('place_changed', () => {
-            this.infoWindow.close();
-            this.marker.setVisible(false);
-            var place = this.autocompletePlaces.getPlace();
-            if (!place.geometry) {
-                // User entered the name of a Place that was not suggested and
-                // pressed the Enter key, or the Place Details request failed.
-                window.alert("No details available for input: '" + place.name + "'");
-                return;
-            }
 
-            // If the place has a geometry, then present it on a map.
-            if (place.geometry.viewport) {
-                this.map.fitBounds(place.geometry.viewport);
-            } else {
-                this.map.setCenter(place.geometry.location);
-                this.map.setZoom(17);  // Why 17? Because it looks good.
-            }
-            this.marker.setIcon(/** @type {google.maps.Icon} */({
-                url: this.MARKER_PATH,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(35, 35)
-            }));
-            this.marker.setPosition(place.geometry.location);
-            this.marker.setVisible(true);
-
-            var address = '';
-            if (place.address_components) {
-                address = [
-                    (place.address_components[0] && place.address_components[0].short_name || ''),
-                    (place.address_components[1] && place.address_components[1].short_name || ''),
-                    (place.address_components[2] && place.address_components[2].short_name || '')
-                ].join(' ');
-            }
-
-            this.infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-            this.infoWindow.open(this.map, this.marker);
+        this.autocompletePlaces.addListener('place_changed', (place: any) => {
+            this.setPlace(this.autocompletePlaces.getPlace());
         });
-        //PLACES_____________________________
-
-
-
-
-
-
-
-        this.autocomplete.addListener('place_changed', () => {
-            var place = this.autocomplete.getPlace();
-            if (place.geometry) {
-                this.map.panTo(place.geometry.location);
-                this.map.setZoom(15);
-            } else {
-                (<HTMLInputElement>document.getElementById('autocomplete')).placeholder = 'Enter a city';
-            }
+        google.maps.event.addListener(this.infoWindow, 'closeclick', () => {
+            this.infoWindow.close()
         });
+
 
         // Add a DOM event listener to react when the user selects a country.
         document.getElementById('country').addEventListener(
@@ -164,47 +104,66 @@ export class AddPlacePart1Component implements OnInit {
 
     }
 
-    searchPlacesByText(query: string, map: any, location: any) {
-        var request = {
-            location: location,
-            radius: '50',
-            query: query
-        };
-        let service = new google.maps.places.PlacesService(map);
-        service.textSearch(request, (results: any, status: any) => {
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    var place = results[i];
-                    //console.log('text search place', place);
-                    //createMarker(results[i]);
-                }
+    setPlace(place: any) {
+        console.log(place);
+
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+        this.placeService.getDetails({ placeId: place.id }, (place: any, status: any) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                place = place;
+
+                this.choosenPlace = place;
+                console.log(this.choosenPlace)
             }
         });
+        // If the place has a geometry, then present it on a map.
+        if (place && place.geometry.viewport) {
+            this.map.fitBounds(place.geometry.viewport);
+        } else {
+            this.map.setCenter(place.geometry.location);
+            this.map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        this.putMarker(place.geometry.location);
+        this.infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' + place.vicinity);
+    }
+
+    putMarker(location: any) {
+        this.infoWindow.close();
+        this.marker.setVisible(false);
+        this.marker.setIcon(/** @type {google.maps.Icon} */({
+            url: this.MARKER_PATH,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        this.marker.setPosition(location);
+        this.marker.setVisible(true);
+        this.infoWindow.open(this.map, this.marker);
     }
 
     getPlacesByLocation(location: any, map: any) {
         // Get places near by
-        var request = {
+        const request = {
             location: location,
-            radius: '50',
-            type: 'restaurant'
+            radius: '10',
+            types: ['restaurant', 'cafe', 'bar', 'meal_takeaway', 'meal_delivery']
         };
-
         let service = new google.maps.places.PlacesService(map);
         service.nearbySearch(request, (results: any, status: any) => {
+            console.log(results);
             if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    var place = results[i];
-                    console.log(place);
-                    // createMarker(results[i]);
+                if (results.length) {
+                    this.setPlace(results[0]);
                 }
             }
         });
-
-
     }
-
-
 
     handleLocationError(browserHasGeolocation: boolean, infoWindow: any, pos: any) {
         infoWindow.setPosition(pos);
