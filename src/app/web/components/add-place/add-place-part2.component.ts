004a7c1bc7183a70968b90cf92964810918c3167
Vitaliy Chorniy <chornij1@gmail.com>
2016-11-19 01:41:44 +0100

    finished working hours ui

--- src/app/web/components/add-place/add-place-part2.component.ts
+++ src/app/web/components/add-place/add-place-part2.component.ts
@@ -24,66 +24,112 @@ export class AddPlacePart2Component implements AfterViewInit {
 
     ngAfterViewInit() {
         this.workingDays.forEach((day: any) => {
-            this.createDefaultDaySlider(day)
+            if (day.hasBreak) {
+                this.createDaySliderWithBreak(day);
+            } else {
+                this.createDefaultDaySlider(day);
+            }
         });
         this.changeStyles();
     }
 
     changeStyles() {
-      $('.noUi-tooltip').css('font-size', '11px');
-      $('.noUi-tooltip').css('padding', '2px');
-      $('.noUi-handle').addClass( 'elp-tooltip' );
+        $('.noUi-tooltip').css('font-size', '11px');
+        $('.noUi-tooltip').css('padding', '2px');
+        $('.noUi-handle').addClass('elp-tooltip');
     }
 
     toogleBreak(day: any) {
-      let selected = this.sliders.find((slider:any) => slider.name === day.name);
-      selected.slider.noUiSlider.destroy();
-      if(day.hasBreak){
-        this.createDaySliderWithBreak(day);
-      } else {
-        this.createDefaultDaySlider(day);
-      }
-      this.changeStyles();
+        let selected = this.sliders.find((slider: any) => slider.name === day.name);
+        selected.slider.noUiSlider.destroy();
+        if (day.hasBreak) {
+            this.createDaySliderWithBreak(day);
+        } else {
+            this.createDefaultDaySlider(day);
+        }
+        this.changeStyles();
+    }
+
+    toogleDayActive(day: any) {
+        let selected = this.sliders.find((slider: any) => slider.name === day.name);
+        if (!day.selected) {
+            selected.slider.setAttribute('disabled', true);
+        } else {
+            selected.slider.removeAttribute('disabled');
+        }
+    }
+
+    updateDay(dayName: string, values: any) {
+        let selectedDay = this.workingDays.find((day: any) => {
+            return day.name === dayName
+        });
+
+        if (values.length === 4) {
+            selectedDay.business_hours.from = values[0];
+            selectedDay.business_hours.to = values[3];
+            selectedDay.break.from = values[1];
+            selectedDay.break.to = values[2];
+        } else {
+            selectedDay.business_hours.from = values[0];
+            selectedDay.business_hours.to = values[1];
+        }
     }
 
     createDefaultDaySlider(day: any) {
-      let slider = (<any>document.getElementById(day.name));
+        const updateDay = this.updateDay;
+        const context = this;
+        let slider = (<any>document.getElementById(day.name));
 
-      noUiSlider.create(slider, {
-          start: [8, 18],
-          connect: [false, true, false],
-          tooltips: true,
-          step: 0.5,
-          margin: 0,
-          range: {
-              'min': 0,
-              'max': 24
-          }
-      });
-      slider.noUiSlider.on('update', function(values: any, handle: any) {
-          console.log(this.target.id, values, handle);
-      });
-      this.sliders.push({ name: day.name, slider: slider});
+        noUiSlider.create(slider, {
+            start: [day.business_hours.from, day.business_hours.to],
+            connect: [false, true, false],
+            tooltips: true,
+            step: 0.5,
+            margin: 0,
+            range: {
+                'min': 0,
+                'max': 24
+            }
+        });
+        slider.noUiSlider.on('update', function(values: any, handle: any) {
+            updateDay.call(context, this.target.id, values)
+        });
+
+        if (!day.selected) {
+            slider.setAttribute('disabled', true);
+        }
+
+        this.sliders.push({ name: day.name, slider: slider });
     }
 
     createDaySliderWithBreak(day: any) {
-      let slider = (<any>document.getElementById(day.name));
+        const updateDay = this.updateDay;
+        const context = this;
+        let slider = (<any>document.getElementById(day.name));
 
-      noUiSlider.create(slider, {
-          start: [8, 12, 13, 18],
-          connect: [false, true, false, true, false],
-          tooltips: true,
-          step: 0.5,
-          margin: 0,
-          range: {
-              'min': 0,
-              'max': 24
-          }
-      });
-      slider.noUiSlider.on('update', function(values: any, handle: any) {
-          console.log(this.target.id, values, handle);
-      });
-      this.sliders.push({ name: day.name, slider: slider});
+        noUiSlider.create(slider, {
+            start: [
+                day.business_hours.from,
+                day.break.from,
+                day.break.to,
+                day.business_hours.to
+            ],
+            connect: [false, true, false, true, false],
+            tooltips: true,
+            step: 0.5,
+            margin: 0,
+            range: {
+                'min': 0,
+                'max': 24
+            }
+        });
+        slider.noUiSlider.on('update', function(values: any, handle: any) {
+            updateDay.call(context, this.target.id, values)
+        });
+        if (!day.selected) {
+            slider.setAttribute('disabled', true);
+        }
+        this.sliders.push({ name: day.name, slider: slider });
     }
 
     goToStep3() {
