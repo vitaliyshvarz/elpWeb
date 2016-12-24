1000bd0a3dee601ff4d32f0f5a11f10e5c696af5
Vitaliy Chorniy <chornij1@gmail.com>
2016-12-24 22:27:58 +0100

    added install typings

--- gulpfile.js
+++ gulpfile.js
@@ -24,87 +24,98 @@ var typescript = require('gulp-typescript');
 /* Images */
 var imagemin = require('gulp-imagemin');
 
-var tsProject = typescript.createProject('tsconfig.json',
-{ experimentalDecorators: true });
+var tsProject = typescript.createProject('tsconfig.json', {
+    experimentalDecorators: true
+});
 
 gulp.task('build-third-js', function () {
-  return gulp.src(['node_modules/foundation-sites/dist/**/*.js'])
-          .pipe(concat('third.js'))
-          .pipe(gulp.dest(prod + 'assets/js'));
+    return gulp.src(['node_modules/foundation-sites/dist/**/*.js'])
+        .pipe(concat('third.js'))
+        .pipe(gulp.dest(prod + 'assets/js'));
 });
 
 gulp.task('build-css', function () {
-  return gulp.src(dev + '**/*.scss')
-      .pipe(sourcemaps.init())
-      .pipe(postcss([precss, autoprefixer, cssnano]))
-      .pipe(sourcemaps.write())
-      .pipe(ext_replace('.css'))
-      .pipe(gulp.dest(prod));
+    return gulp.src(dev + '**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([precss, autoprefixer, cssnano]))
+        .pipe(sourcemaps.write())
+        .pipe(ext_replace('.css'))
+        .pipe(gulp.dest(prod));
 });
 
 gulp.task('build-ts', function () {
-  return gulp.src([dev + '**/*.ts', 'typings/tsd.d.ts'])
-      .pipe(sourcemaps.init())
-      .pipe(typescript(tsProject))
-      .pipe(jsuglify())
-      .pipe(sourcemaps.write('../../' + prod))
-      .pipe(gulp.dest(prod));
+    return gulp.src([dev + '**/*.ts', 'typings/tsd.d.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsProject))
+        .pipe(jsuglify())
+        .pipe(sourcemaps.write('../../' + prod))
+        .pipe(gulp.dest(prod));
 });
 
 gulp.task('copy-vid', function () {
-  return gulp.src([dev + '**/*.mp4', dev + '**/*.webm'])
-      .pipe(gulp.dest(prod));
+    return gulp.src([dev + '**/*.mp4', dev + '**/*.webm'])
+        .pipe(gulp.dest(prod));
 });
 
 gulp.task('build-img', function () {
-  return gulp.src([dev + '**/*.jpg', dev + '**/*.png'])
-      .pipe(imagemin({
-        progressive: true
-      }))
-      .pipe(gulp.dest(prod));
+    return gulp.src([dev + '**/*.jpg', dev + '**/*.png'])
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest(prod));
 });
 
 gulp.task('build-html', function () {
-  return gulp.src(dev + '**/*.html')
-    .pipe(gulp.dest(prod));
+    return gulp.src(dev + '**/*.html')
+        .pipe(gulp.dest(prod));
 });
 
 gulp.task('watch', function () {
-  gulp.watch([dev + '**/*.ts',
-              dev + '**/*.scss',
-              dev + '**/*.html',
-              dev + '**/*.png',
-              dev + '**/*.jpg'], [
-                'build-ts',
-                'build-css',
-                'build-html',
-                'build-img'
-              ]);
+    gulp.watch([dev + '**/*.ts',
+        dev + '**/*.scss',
+        dev + '**/*.html',
+        dev + '**/*.png',
+        dev + '**/*.jpg'
+    ], [
+        'build-ts',
+        'build-css',
+        'build-html',
+        'build-img'
+    ]);
 });
 
 gulp.task('clean', function () {
-  return gulp.src('dist', {read: false})
-      .pipe(clean());
+    return gulp.src('dist', {
+            read: false
+        })
+        .pipe(clean());
 });
 
 gulp.task('start', function () {
-  runSequence('develop',
-              'build-third-js',
-              'copy-vid',
-              'watch');
+    runSequence('develop',
+        'build-third-js',
+        'copy-vid',
+        'watch');
+});
+
+gulp.task('build-prod', function () {
+    runSequence('develop',
+        'build-third-js',
+        'copy-vid');
 });
 
 gulp.task('develop', function (done) {
-  runSequence(
-  'clean',
-  'build-ts',
-  'build-css',
-  'build-html',
-  'build-img', function () {
-    done();
-  });
+    runSequence(
+        'clean',
+        'build-ts',
+        'build-css',
+        'build-html',
+        'build-img',
+        function () {
+            done();
+        });
 });
 
 gulp.task('default', [
-  'start'
+    'start'
 ]);
