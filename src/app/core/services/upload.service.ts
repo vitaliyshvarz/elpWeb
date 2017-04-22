8981335621193b6ab1a54e34740d6f3317ec18c2
Vitaliy Chorniy <chornij1@gmail.com>
2017-04-22 09:35:51 +0200

    identation fixed

--- src/app/core/services/upload.service.ts
+++ src/app/core/services/upload.service.ts
@@ -13,9 +13,9 @@ export class UploadService {
     progress$: any;
     progressObserver: any;
     constructor(
-      private http: Http,
-      private alertService: AlertService,
-      private sessionService: SessionService
+        private http: Http,
+        private alertService: AlertService,
+        private sessionService: SessionService
     ) {
         this.progress$ = Observable.create((observer: any) => {
             this.progressObserver = observer;
@@ -24,16 +24,16 @@ export class UploadService {
 
     uploadImage(params: string[], fileList: File[]) {
         if (fileList.length > 0) {
-               let file: File = fileList[0];
-               let formData: FormData = new FormData();
-               formData.append('uploadFile', file, file.name);
-               formData.append('extention', file.name.split('.').pop());
-               let headers = new Headers();
-               headers.append('Content-Type', 'multipart/form-data');
-               headers.append('Accept', 'application/json');
-               return this.http.post(BACKEND_API.uploadImage, formData, this.sessionService.addTokenHeader())
-                   .map(res => res.json())
-                   .catch(error => Observable.throw(error));
-           }
+            let file: File = fileList[0];
+            let formData: FormData = new FormData();
+            formData.append('uploadFile', file, file.name);
+            formData.append('extention', file.name.split('.').pop());
+            let headers = new Headers();
+            headers.append('Content-Type', 'multipart/form-data');
+            headers.append('Accept', 'application/json');
+            return this.http.post(BACKEND_API.uploadImage, formData, this.sessionService.addTokenHeader())
+                .map(res => res.json())
+                .catch(error => Observable.throw(error));
+        }
     }
 }
